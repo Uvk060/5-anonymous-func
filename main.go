21abe3c75e3aa2525ee099962d013d58551025c3
Uvk060 <uvk060@gmail.com>
2020-12-14 16:27:37 +0200

    init

--- main.go
+++ main.go
@@ -7,9 +7,23 @@ import (
 
 func main() {
 	people := []string{"Alice", "Bob", "Dave"}
-	sort.Slice(people, func(i, j int) bool {
-		return len(people[i]) < len(people[j])
-	})
+	fruits := []string{"Banana", "Orange", "Apple"}
+	ex := []string{"bnklbnkl", "vjff", "kgf"}
 
-	fmt.Println(people)
+	fmt.Println(sortByLen(people))
+	sortByLenAndPrint(fruits)
+	sortByLenAndPrint(ex)
+}
+
+// sort and return sorted data
+func sortByLen(list []string) []string {
+	sort.Slice(list, func(i, j int) bool {
+		return len(list[i]) < len(list[j])
+	})
+	return list
+}
+
+// Print on screen sorted items
+func sortByLenAndPrint(list []string) {
+	fmt.Println(sortByLen(list))
 }
